0b9cd1ee633905e1c7ad7c35c53e4541a7d9e68a
HeeTaeKim996 <ttiiger1@naver.com>
2025-02-14 12:34:34 +0900

    FirstUpload

---
+++ My/ExpansionMethod.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using hellWorld;
+
+namespace My
+{
+    public static class ExpansionMethod
+    {
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            #region 공부정리
+            /*
+            ○ this
+            - this는 this가 위치에 따라 역할이 달라짐
+            - 클래스 내부에 위치한 경우, this는 클래스의 인스턴스를 지칭
+            - 매서드 내부(매개변수)에 위치한 경우, this는 해당 매서드를 구현하는 객체를 지칭. (Ex. a.MethodA(this..) 에서 this는 a를 지칭
+
+            ○ Static
+            - 매개변수에 this를 사용하는 매서드를 확장 매서드라 함
+            - 확장매서드라 불리는 이유는, this를 사용하여 a.Method(this..); 의 형태가, a.GetType == Class A라 했을 때, A의 매서드를 사용하는 형태와 동일하기 때문,
+              실제로는 확장매서드가 this의 대상이 되는 객체의 클래스의 자체 매서드가 아님에도, 자체매서드의 사용 형태와 동일하기 때문
+             ※ 확장매서드의 이러한 성질에 의해, 확장매서드를 담고 있는 클래스는, Generic이 아닌, 정적 클래스여야 함(앞에 static이 있는)
+             - 만약 static이 없는 generic 클래스이고, 확장매서드가 내부에 정의됐다면, 클래스 A의 인스턴스를 a라 했을 때, 그리고 확장 매서드를 구현하는 객체를 b라 했을 때, 
+               인스턴스 a를 사용하여 확장매서드를 구현하는 형태는, a.b.확장매서드(this..) 의 형태가 되기 때문에, 이를 방지하고자, 
+               매개변수에 this를 담은 확장매서드를 담은 클래스는 앞에 static이 위치한, 인스탠스화가 불가능한 정적 클래스여야함
+
+            ○ INumerable<T>
+            - 위 this의 대상이 되는 매서드 구현 객체는, INumerable<T>를 인터페이스로 구현 가능해야 함. int, List .. 등은 INumerable<T>를 인터페이스로 구현하기 때문에, 
+              구현 객체가 될 수 있음
+            */
+            #endregion
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (T t in source)
+            {
+                if (predicate(t))
+                {
+                    yield return t;
+                }
+            }
+        }
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(IEnumerable<TSource>source, Func<TSource, TResult> function)
+        {
+            if(source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if(function == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach(TSource t in source)
+            {
+                yield return function(t);
+            }
+        }
+        public static void MyForeach<T>(this List<T> source, Action<T> function)
+        {
+            #region 공부정리
+            /*
+            위에 Action이 왔는데, 
+             - 반환값이 있는 Delegate는, Func<매개변수1,매개변수2...,매개변수n, 리턴변수> ; 를 사용
+             - 반환값이 없는 Delegate는, Action<매개변수1,매개변수2,...,매개변수n>; 를 사용
+            */
+            #endregion
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (function == null)
+            {
+                throw new ArgumentNullException(nameof(function));
+            }
+
+            foreach (T t in source)
+            {
+                function(t);
+            }
+        }
+        public static T MySum<T>(this IEnumerable<T> source) where T : struct, INumber<T>
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            T sum = T.Zero;
+
+            foreach (T t in source)
+            {
+                sum += t;
+            }
+            return sum;
+        }
+        public static int MyCount<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            int count = 0;
+
+            foreach (T t in source)
+            {
+                count++;
+            }
+
+            return count;
+        }
+        public static float MyAverage<T>(this IEnumerable<T> source) where T : struct, INumber<T>
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            int count = 0;
+            T sum = T.Zero;
+
+            foreach (T t in source)
+            {
+                count++;
+                sum += t;
+            }
+
+            return Convert.ToSingle(sum) / count;
+        }
+        public static IEnumerable<T> MyOrderBy<T>(this IEnumerable<T> source, Func<T, T> function) where T : struct, INumber<T>
+        {
+            // 개선이 많이 필요한 매서드인듯
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            List<T> resultList = new List<T>();
+
+            foreach (T t in source)
+            {
+                T shiftT = function(t);
+                bool checkBool = false;
+
+                for (int i = 0; i < resultList.Count; i++)
+                {
+                    if (shiftT <= function(resultList[i]))
+                    {
+                        resultList.Insert(i, t);
+                        checkBool = true;
+                        break;
+                    }
+                }
+
+                if (!checkBool)
+                {
+                    resultList.Add(t);
+                }
+            }
+            return resultList;
+        }
+        public static T MyMax<T>(this IEnumerable<T> source) where T : struct, IComparable<T>
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            T resultT = source.First();
+
+            foreach (T t in source)
+            {
+                if (resultT.CompareTo(t) < 0)
+                {
+                    resultT = t;
+                }
+            }
+
+            return resultT;
+        }
+        public static T MyNear<T>(this IEnumerable<T> source, T pivotValue) where T : struct, INumber<T>
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            T returnT = source.First();
+            T distance = returnT - pivotValue;
+            T absDistance = T.Abs(distance);
+
+            foreach (T t in source)
+            {
+                distance = t - pivotValue;
+                T absNewDistance = T.Abs(distance);
+                if (absDistance > absNewDistance)
+                {
+                    returnT = t;
+                    absDistance = absNewDistance;
+                }
+            }
+
+            return returnT;
+        }
+        public static IEnumerable<T> MyUnion<T>(this IEnumerable<T> source1, IEnumerable<T> source2) where T : struct
+        {
+            foreach (T t in source1)
+            {
+                yield return t;
+            }
+            foreach (T t in source2)
+            {
+                yield return t;
+            }
+        }
+        public static IEnumerable<IGrouping<TCoin, TSource>> MyGroupBy<TCoin, TSource>(this IEnumerable<TSource> source, Func<TSource, TCoin> function)
+        {
+            #region 공부정리
+            /*
+            ○ IGrouping
+             - 매서드의 리턴값이 IENumerable<IGrouping<Tkey, TSource> 인데, 이렇게 사용하면 리턴값은 TKey는 Dictionary의 Key와 같은 역할이고, TSource는 IEnumerable<TSource>형식으로 리턴함
+             - Dictionary는 C 초창기?에 나온 컬렉션이고, IENumerable<IGrouping.. 은 System.Collections 에서 정의됨
+             - 매서드의 리턴값이 IENumerable<IGrouping.. 일시, 매서드 내 yield return new ClasA 형식으로 리턴해야하며, ClassA는 Interface IGrouping 을 상속한 클래스여야 함
+            */
+            #endregion
+            Dictionary<TCoin, List<TSource>> dictionary = new Dictionary<TCoin, List<TSource>>();
+
+            foreach (TSource t in source)
+            {
+                TCoin tCoin = function(t);
+                if (!dictionary.ContainsKey(tCoin))
+                {
+                    dictionary[tCoin] = new List<TSource>();
+                }
+                dictionary[tCoin].Add(t);
+            }
+
+            foreach (var kvp in dictionary)
+            {
+                yield return new MyGrouping<TCoin, TSource>(kvp.Key, kvp.Value);
+            }
+        }
+    }
+
+}
---
+++ My/Program.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections;
+
+namespace hellWorld
+{
+    public class MyGrouping<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        // 클래스에 인터페이스 상속 및 클래스 내에 public ClasName으로 변수명 다시 정의하는 것은 추후 공부할 것 같으므로, 이 내용은 공부 생략함
+        public TKey Key { get; }
+        public IEnumerable<TElement> Elements { get; }
+
+        public MyGrouping(TKey key, IEnumerable<TElement> elements)
+        {
+            Key = key;
+            Elements = elements;
+        }
+        public IEnumerator<TElement> GetEnumerator() => Elements.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        // 위 매서드가 무슨 역할인지는 모르겠고.. 일단 IGrouping을 구현하기 위해, 매서드를 상속해야 해서 입력함
+    }
+
+    internal class My
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Sort_HeapSort(a, (a, b) => a.CompareTo(b));
+            foreach(int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        /// <summary>
+        /// 두 실수의 최대공약수를 반환하는 함수
+        /// </summary>
+        /// <typeparam name="T">IComparable, IConvertible을 만족하는 실수</typeparam>
+        /// <param name="firstInt">첫번째 비교인자</param>
+        /// <param name="secondInt">두번째 비교인자</param>
+        /// <returns>최대공약수를 반환</returns>
+        static T GetGCD<T>(T firstInt, T secondInt) where T : struct, INumber<T>
+        {
+            T zero = T.Zero;
+            if(firstInt < zero)
+            {
+                firstInt = -firstInt;
+            }
+            if(secondInt < zero)
+            {
+                secondInt = -secondInt;
+            }
+
+            if(firstInt < secondInt)
+            {
+                (firstInt, secondInt) = (secondInt, firstInt);
+            }
+
+
+            if(secondInt == zero)
+            {
+                return firstInt;
+            }
+
+            T remainder;
+
+            do
+            {
+                remainder = firstInt % secondInt;
+                firstInt = secondInt;
+                secondInt = remainder;
+            } while (remainder != zero);
+
+            return firstInt;
+            #region Reference
+            /* 참조
+             * pri.ba.0 - 유클리드 호제법 (Euclidian Algorithm)
+             * 유클리드 호제법을 사용하여, A = 몫 * B + R; A = B; B = R; 을 반복하여, R = 0이 될 때에 A = 몫 & B + R 에서의 B가 최대공약수  
+             * BigInter.GreatestCommonDivosor(BigInter, BigInter) 함수를 참조하여 만듬 - a b 가 음수일 시 양으로 변환. a == 0 || a == b 일시 Max(a,b) 처리
+             */
+            #endregion
+        }
+        /// <summary>
+        /// 두 실수의 최소공배수를 반환하는 함수
+        /// </summary>
+        /// <typeparam name="T">struct, IComparable, IConvertible을 만족하는 실수</typeparam>
+        /// <param name="firstInt">첫버째 인수</param>
+        /// <param name="secondInt">두번째 인수</param>
+        /// <returns>두 실수의 최소공배수를 반환</returns>
+        static T GetLCM<T>(T firstInt, T secondInt) where T : struct, INumber<T>
+        {
+            T returT = firstInt / GetGCD(firstInt, secondInt) * secondInt;
+            if(returT < T.Zero)
+            {
+                returT = -returT;
+            }
+            return returT;
+            #region Reference
+            /*
+             * 참조 - pri-ba-0.유클리드 호제법 - EuclidianAlgorithm
+             * 참조 - pri-ba-1. GCD - LCM 관계 
+             * LCM 을 구하는 기본 제공 매서드는 없다함. abs( a * b ) / BiggerInt.GreatestCommonDivisor(a,b); 로 구한다함
+             * 그래서 한 수가 0이면, LCM은 0으로 처리하는듯
+             * */
+            #endregion
+        }
+        /// <summary>
+        /// 최솟값을 반환하는 함수
+        /// </summary>
+        /// <typeparam name="T">struct, IComparable<T>를 만족하는 실수</T></typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>최솟값을 반환</returns>
+        static T Min<T>(T first, T second) where T : struct, IComparable<T> // where 우측에 <T> 를 입력하면(Generic Inteface), 1) T타입과 동일한 타입의 객체와만 비교하도록 강제하며, 2) 컴파일러가 타입을 미리 알기 때문에 성능 면에서 유리. 3) 잘못된 타입과 비교하는 실수 방지
+        {
+            return first.CompareTo(second) <= 0 ? first : second;
+            // A.CompareTo(B) : 1) A > B : 1반환, 2) A == B : 0반환, 3) A < B : -1 반환
+        }
+        /// <summary>
+        /// 최대값을 반환하는 함수
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>최대값을 반환</returns>
+        static T Max<T>(T first, T second) where T : struct, IComparable<T>
+        {
+            return first.CompareTo(second) >= 0 ? first : second;
+        }
+        /// <summary>
+        /// 절대값을 반환하는 함수
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        static T Abs<T>(T value) where T : struct, INumber<T>
+        {
+            return value < T.Zero ? -value : value;
+        }
+        public static void MySort_SimpleComparisionSort<T>(T[] source, Func<T, T, int> function) where T : IComparable<T>
+        {
+            #region 공부정리
+            /*
+            □
+            이 매서드처럼(원매서드 : Array.Sort 인 정적매서드), 매개변수가 참조형식인 클래스인 경우에, 해당 매개변수를 수정하면, 그 매개변수 값이 바뀐다.
+            이를 활용하여, 정적매서드를 호출하는 매서드가 외부 클래스에 위치하더라도, 정적매서드로 변경된 클래스는 영구적으로 매서드를 호출한 외부클래스에서도 매개변수 클래스 내의 값들이 변경된다.
+            => 이를 활용하면, EnemyMovement 클래스를 여러 계층으로 상속 처리하는 위험을 감수하지 않더라도, void를 반환하는 정적매서드에서 struct 이 아닌 class를 변경하는 것으로, 개별 EnemyMovement 클래스를 간소화하는 것이 가능할듯
+            □
+            이 정렬법은 단순하지만, 연산효율이 좋지는 않은 연산법
+             */
+            #endregion
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            for (int i = 0; i < source.Length; i++)
+            {
+                for (int j = i + 1; j < source.Length; j++)
+                {
+                    if (function(source[i], source[j]) > 0)
+                    {
+                        (source[i], source[j]) = (source[j], source[i]);
+                    }
+                }
+            }
+        }
+
+        #region QuickSortMethods
+        public static void MySort_QuickSort<T>(T[] source, Func<T, T, int> function) where T : IComparable<T>
+        {
+            #region 공부정리
+            /*
+            □ Sort 연산법 중 하나인 QuickSort
+             - 우선 임의의 피벗 요소 [코드에서는 파티션에서 가장 우측에 위치한 요소-for(..)처리하기 편하니] 를 고르고, 그 값보다 작으면 좌측, 크면 우측, 이 가운데에 피벗 요소를 배치
+             - 이렇게 나뉘 왼쪽, 오른쪽 파티션에 동일한 작업을 반복하여 정렬
+            */
+            #endregion
+            QuickSort(source, 0, source.Length - 1, function);
+
+        }
+        public static void QuickSort<T>(T[] array, int left, int right, Func<T, T, int> function) where T : IComparable<T>
+        {
+            if (left < right)
+            {
+                int pivotIndex = QuickSort_Partition(array, left, right, function);
+                QuickSort(array, left, pivotIndex - 1, function);
+                QuickSort(array, pivotIndex + 1, right, function);
+            }
+        }
+        public static int QuickSort_Partition<T>(T[] array, int left, int right, Func<T, T, int> function) where T : IComparable<T>
+        {
+            T pivot = array[right];
+
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (function(array[j], pivot) <= 0)
+                {
+                    i++;
+                    (array[i], array[j]) = (array[j], array[i]);
+                }
+            }
+
+            (array[i + 1], array[right]) = (array[right], array[i + 1]);
+
+            return i + 1;
+        }
+        #endregion
+
+        #region Sort_HeapSort
+        public static void Sort_HeapSort<T>(T[] array, Func<T, T, int> function) where T : struct, IComparable<T>
+        {
+            #region 공부정리
+            /*
+            ■ Heap정렬법
+            □ 작동원리
+            ○ 기본 구조
+             - Heap정렬은 index 0 이 자식 index 1, index 2를 두고, index 1 는 index 3, 4를, index 2 는 index 5,6을 자식으로 둔다. 즉 0으로 시작한, 2개의 자식을 두는 구조가 무한히 반복된다.
+             - 이렇게 자식을 둔다 하면, index n의 자식의 index값은, 2*n + 1, 2*n+2 가 된다. (이것 관련 수리증명은 아직 못했지만, index를 쭉 나열해보면, 확인은 가능..)
+            ○ Heapify
+             - 위의 구조에서, Hepaify(index: n)을 한다는 것은, index n과, n의 자식 2*n+1, 2*n+2중 가장 큰 값을 n의 위치와 스왑한다.
+             - 스왑이 이뤄질시(기존부모n이 자식 중 하나보다 작았을시), 스왑된 자식의 기존 위치에서(2*n+1 또는 2*n+2의 위치), 다시 재귀적으로 Heapify를 시행한다
+            ○ 작동 전개과정
+             1) index 0 부터 n / 2 - 1 [ (n/2-1) * 2 + 2 = n이므로] 까지의 index를 대상으로, n/2-1 부터, 0까지 내려오며, 순차적으로 재귀적 Heapify를 시행한다 [이 과정을 Building Max Heap 이라 하는듯]
+             2) Building Max Heap 처리후, index 0과, index 최댓값을 스왑한다. 이후, index 최댓값을 Heapify 대상에서 제외하고, 재귀적 Heapify(index 0) 를 시행한다 -> 이 과정을 Heapify대상이 없어지는, 1 아래 코드의 length 가 1이 될 때까지 반복 시행한다
+            */
+            #endregion
+            int length = array.Length;
+
+            for (int i = length / 2 - 1; i >= 0; i--)
+            {
+                HeapSort_Heapify(array, length, i, function);
+            }
+
+            for (int i = length - 1; i > 0; i--)
+            {
+                (array[0], array[i]) = (array[i], array[0]);
+                HeapSort_Heapify(array, i, 0, function);
+            }
+        }
+        private static void HeapSort_Heapify<T>(T[] array, int length, int parentIndex, Func<T, T, int> function) where T : struct, IComparable<T>
+        {
+            int largest = parentIndex;
+            int leftChildIndex = parentIndex * 2 + 1;
+            int rightChidIndex = parentIndex * 2 + 2;
+
+            if (leftChildIndex < length && function(array[largest], array[leftChildIndex]) < 0)
+            {
+                largest = leftChildIndex;
+            }
+            if (rightChidIndex < length && function(array[largest], array[rightChidIndex]) < 0)
+            {
+                largest = rightChidIndex;
+            }
+
+            if (largest != parentIndex)
+            {
+                (array[parentIndex], array[largest]) = (array[largest], array[parentIndex]);
+                HeapSort_Heapify(array, length, largest, function);
+            }
+        }
+        #endregion
+
+        static int MyBinarySearch<T>(T[] array, T searchingValue) where T : struct, IComparable<T>
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (right - left > 1)
+            {
+                int averageIndex = left + (right - left) / 2; // OverFlowPrevention;
+                int compareInt = searchingValue.CompareTo(array[averageIndex]);
+                if (compareInt == 0)
+                {
+                    return averageIndex;
+                }
+                else if (compareInt > 0)
+                {
+                    left = averageIndex + 1;
+                }
+                else
+                {
+                    right = averageIndex - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
